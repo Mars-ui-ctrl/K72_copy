65e3cd66488a9906f9f1afc3ed4e8ab8417ca3cb
master <jitansaw0416@gmail.com>
2026-08-19 22:23:18 +0530

    optimizing images

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -17,14 +17,6 @@ const Agence = () => {
   const imageRef = useRef(null);
 
   const imageCollection = [
-    // "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
-    // "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64",
-    // "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
-    // "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
-    // "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
-    // "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
-    // "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
-    // "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6"
     Carl,
     Olivier,
     ChantalG,
@@ -35,12 +27,12 @@ const Agence = () => {
     Joel,
   ]
 
+  imageCollection.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+  });
   gsap.registerPlugin(ScrollTrigger);
   useGSAP(() => {
-    imageCollection.forEach((src) => {
-      const img = new Image();
-      img.src = src;
-    });
     gsap.to(imageDivRef.current, {
       scrollTrigger: {
         trigger: imageDivRef.current,
@@ -65,7 +57,7 @@ const Agence = () => {
     <div>
       <div className="agence-section1">
         <div ref={imageDivRef} className="absolute top-[25%] overflow-hidden left-[30%] ">
-          <img ref={imageRef} className="h-80 object-cover rounded-2xl z- " src={Carl}alt="agence-img1" />
+          <img ref={imageRef} className="h-80 object-cover rounded-2xl z- " src={Carl} alt="agence-img1" />
         </div>
         <div>
 
